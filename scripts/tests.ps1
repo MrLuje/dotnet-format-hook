e89abbe3fd3f69a456b7811022a70b3feaf354f6
MrLuje <ogmniluje@gmail.com>
2020-06-02 15:38:46 +0200

    MOAR LOGS

--- scripts/tests.ps1
+++ scripts/tests.ps1
@@ -8,7 +8,12 @@ if ((Test-Path $out)) {
 }
 
 New-Item $out -ItemType Directory > $null
-Remove-Item -Path ([IO.Path]::Combine("$env:USERPROFILE", ".nuget", "packages", "format-hook")) -Recurse -ErrorAction SilentlyContinue
+$nugetFormatHookPath = $(if ($null -ne $env:USERPROFILE) { ([IO.Path]::Combine("$env:USERPROFILE", ".nuget", "packages", "format-hook")) } else { ([IO.Path]::Combine("$env:HOME", ".nuget", "packages", "format-hook")) })
+
+if ($env:VERBOSE -eq "true") {
+    Write-Host "Removing nuget at $nugetFormatHookPath"
+}
+Remove-Item -Path $nugetFormatHookPath -Recurse -Force -ErrorAction SilentlyContinue
 
 dotnet pack ([IO.Path]::Combine("..", "src", "format-hook")) -o $out
 $pkgs = Get-ChildItem $out -Filter "*.nupkg" -Recurse
@@ -18,14 +23,13 @@ if ($pkgs.Length -eq 0) {
     Return
 }
 
-$pkg = $pkgs[0].Name
 $pkgFolder = $pkgs[0].Directory.FullName
 $currentPath = Get-Location
-Write-Host "Using package $pkg"
+Write-Host "Using package $($pkgs[0].FullName)"
 
 Copy-Item -Path (Join-Path ".." "tests") -Destination "..\out" -Recurse
 
-Get-ChildItem ([IO.Path]::Combine("..", "out", "tests")) |
+Get-ChildItem ([IO.Path]::Combine("..", "out", "tests")) -Directory |
 ForEach-Object {
     $testName = $_.Name
     $testFullpath = $_.FullName
@@ -33,20 +37,23 @@ ForEach-Object {
     Set-Location -Path $testFullpath
 
     Write-Host "** Testing '$testName' " -NoNewline
-    $success = Invoke-Command { & (Join-Path -Path $testFullpath -ChildPath "test.ps1") -pkg "format-hook" -pkgSrc $pkgFolder -testName $testName }
+    $result = Invoke-Command { & (Join-Path -Path $testFullpath -ChildPath "test.ps1") -pkg "format-hook" -pkgSrc $pkgFolder -testName $testName }
     Set-Location -Path $currentPath
 
+    $success = if ($result.Length -gt 1) { $result | Select-Object -Last 1 } else { $result }
     switch ($success) {
         { $_ -eq $true } { 
-            Write-Host -ForegroundColor "GREEN" " OK"; break 
+            Write-Host -ForegroundColor "GREEN" " OK";
             exit 0;
         }
         { $_ -eq $false } { 
-            Write-Error "Test '$testName' failed"; break 
+            Write-Host $result;
+            Write-Error "Test '$testName' failed"; 
+            break;
         }
         Default { 
-            Write-Error "Test '$testName' failed"
-            Write-Host $success
+            Write-Host $result;
+            Write-Error "Test '$testName' failed";
         }
     }
     exit 1;
--- tests/sln/test.ps1
+++ tests/sln/test.ps1
@@ -13,34 +13,55 @@ $main = {
     try {
         "       " + (Get-Content (Join-Path "$testName" "Class1.cs") -Raw) | Set-Content (Join-Path "$testName" "Class1.cs")
         
-        git init -q
-        git config user.email "a@a.com"
-        git config user.name "hey"
+        if ($env:VERBOSE -eq "true") {
+            Execute-Command "git" "init"
+        }
+        else {
+            Execute-Command "git" "init -q"
+        }
+        Execute-Command "git" "config user.email 'a@a.com'"
+        Execute-Command "git" "config user.name 'hey'"
+
+        if ((Test-Path ".git") -eq $false) {
+            Write-Error "Git repo in test wasn't initialized"
+            return $false
+        }
         
-        dotnet add (Join-Path "$testName" "$testname.csproj") package $pkg -s $pkgSrc > $null
-        if ($LASTEXITCODE -ne 0) {
+        $pathToCsProj = Join-Path "$testName" "$testname.csproj"
+        $r = Execute-Command "dotnet" "add $pathToCsProj package $pkg -s $pkgSrc"
+        if ($r.ExitCode -ne 0) {
             Write-Error "Failed to install $pkg"
+            # Write-Output $r.stderr
+            # Write-Output $r.stdout
             return $false
         }
 
-        $r = Execute-Command "dotnet" "build" (Get-Location)
+        if ($env:VERBOSE -eq "true") {
+            Write-Output "Executing dotnet build"
+        }
+        
+        $r = Execute-Command "dotnet" "build /flp:v=diag"
 
         if ($r.ExitCode -eq 0) {
             if ((Test-Path (Join-Path ".git" "format-hook.enabled")) -eq $false) {
                 Write-Error "$testName\.git\format-hook.enabled is missing"
-                Write-Output $r.stderr
+                Write-Output $r.stdout
                 return $false
             }
         }
         else {
-            Write-Error "Missing .git\format-hook.enabled !"
+            Write-Error "dotnet build failed"
+            # Write-Output $r.stdout
+            return $false
         }
 
         git add .
-        $r = Execute-Command "git" "commit -m format" (Get-Location)
+        $r = Execute-Command "git" "commit -m format"
 
         if ($r.ExitCode -eq 0) {
             Write-Error "Git commit wasn't prevented by hook :("
+            Write-Information "Re-running hooks in debug"
+            # bash -x "hooks/pre-commit"
         }
         elseif ($r.ExitCode -eq 2) {
             # files formatted
@@ -56,13 +77,20 @@ $main = {
         }
     }
     catch {
-        # Remove-Item -Path .git -Force -Recurse
+        $ErrorMessage = $_.Exception.Message
+        Write-Output $ErrorMessage
     }
     return $false
 }
 
-Function Execute-Command ($commandPath, $commandArguments, $path) {
+Function Execute-Command ($commandPath, $commandArguments, $path = (Get-Location)) {
     Try {
+        if ($env:VERBOSE -eq "true") {
+            Write-Host "Calling " -NoNewline
+            Write-Host "$commandPath $commandArguments" -NoNewline -ForegroundColor "GREEN"
+            Write-Host " from $path... " -NoNewline
+        }
+
         $pinfo = New-Object System.Diagnostics.ProcessStartInfo
         $pinfo.FileName = $commandPath
         $pinfo.RedirectStandardError = $true
@@ -83,6 +111,24 @@ Function Execute-Command ($commandPath, $commandArguments, $path) {
     }
     Catch {
     }
+
+    if ($env:VERBOSE -eq "true") {
+        if ($p.ExitCode -eq 0) {
+            Write-Host "done"
+            if ($p.stdout -ne "") {
+                Write-Host $p.stdout
+            }
+            if ($p.stderr -ne "") {
+                Write-Host $p.stderr
+            }
+        }
+        else {
+            Write-Host "failed !" -ForegroundColor "RED"
+            Write-Host $p.stdout
+            Write-Host $p.stderr
+        }
+    }
+
     $p
 }
 

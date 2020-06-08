c137b0d27de6e652e9966a815091ae7c553d5724
MrLuje <ogmniluje@gmail.com>
2020-06-08 16:11:05 +0200

    test: add multiple-file test

---
+++ tests/multiple-files/multiple-files/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sln
+{
+    public class Class2
+    {
+        public Class2()
+        {
+                    Console.WriteLine("Not properly formatted");
+        }
+    }
+}
